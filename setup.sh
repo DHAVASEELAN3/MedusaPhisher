efe4ee8456009cdf88ca0e9e144cedc5541e9ee6
DHAVASEELAN3 <dhavasee5126@gmail.com>
2025-10-02 10:38:00 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-
 # Function to install dependencies for Debian-based systems
 install_debian_dependencies() {
     sudo apt update
     sudo apt install -y xterm php openssh-client
 }
-
 # Function to install dependencies for Arch-based systems
 install_arch_dependencies() {
     sudo pacman -Syu --noconfirm
     sudo pacman -S --noconfirm xterm php openssh
 }
-
 # Function to determine the user's operating system
 get_os() {
     if [ -e "/etc/os-release" ]; then
@@ -27,9 +24,7 @@ get_os() {
         echo "unsupported"
     fi
 }
-
 # Main script starts here
-
 # Check the OS and install dependencies accordingly
 os=$(get_os)
 if [ "$os" = "debian" ]; then
